6bbe3dba4cdc646a2a752d54349501bc131e7714
Essam Joubori <essam.joubori@gmail.com>
2014-05-22 21:31:12 -0500

    Added hover jq to blog image

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Ecom::Application.routes.draw do
   get "pages/about"
   get "pages/services"
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
