784dd16ffedb52b86a50b04c7db1faea4d4d5754
Essam Joubori <essam.joubori@gmail.com>
2014-07-09 11:38:54 -0400

    Added 's' to HTTP google fonts

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Ecom
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
