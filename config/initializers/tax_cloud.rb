23c7b5d8bb732de00b4b8ace63ba0ba4fa531539
Essam Joubori <essam.joubori@gmail.com>
2014-07-24 20:57:19 -0500

    Finalized store view

--- config/initializers/tax_cloud.rb
+++ config/initializers/tax_cloud.rb
@@ -1,5 +1,5 @@
-TaxCloud.configure do |config|
-  config.api_login_id = 'C1C960'
-  config.api_key = 'EFB92EF9-7F5F-4B79-8978-5B1A05A82A67'
-  config.usps_username = 'your_usps_username'
-end
+# TaxCloud.configure do |config|
+#   config.api_login_id = 'C1C960'
+#   config.api_key = 'EFB92EF9-7F5F-4B79-8978-5B1A05A82A67'
+#   config.usps_username = 'your_usps_username'
+# end
