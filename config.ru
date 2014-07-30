96ca143be88338626f09ad37e8afa863d0e5c2aa
Essam Joubori <essam.joubori@gmail.com>
2014-07-29 23:24:24 -0500

    Store and blog were finalized

--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+require 'rubygems'
+
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ENV['GEM_PATH'] = '/home4/aesthez6/ruby/gems:/lib/ruby/gems/2.0.0'
 
 # Initialize the Rails application.
 Ecom::Application.initialize!
