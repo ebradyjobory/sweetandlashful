6b2fb0e3bc790f62065437cd5f2a06fe13cf3bd4
Essam Joubori <essam.joubori@gmail.com>
2014-07-31 19:13:46 -0500

    Fresh copy of the site

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,7 @@
+class ServicesController < ApplicationController
+
+	layout 'services'
+	def index
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -32,8 +32,7 @@ Ecom::Application.routes.draw do
   get "pages/blog"
   get "pages/about"
   get "pages/contact"
-  get "pages/services"
-
+  match "/services", to: 'services#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
