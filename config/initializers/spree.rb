bc195864a1a5f6318157847efd047e62168f8536
rtimothybrady <rtimothybrady@gmail.com>
2014-08-04 16:12:35 -0400

    Add amazon s3 config for spree

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,9 +6,14 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.use_s3 = true
+  config.s3_bucket = 'sweetandlashful'
+  config.s3_access_key = "AKIAJUSY42V3UMCL4FUA"
+  config.s3_secret = "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"
+end
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
 end
 
 Spree.user_class = "Spree::LegacyUser"
