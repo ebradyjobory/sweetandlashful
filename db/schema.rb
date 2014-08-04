6da19cd8afd8640cf92b35c31e8083066c5322d8
rtimothybrady <rtimothybrady@gmail.com>
2014-08-04 19:33:47 -0400

    played around with spree

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630213356) do
+ActiveRecord::Schema.define(version: 20140804230504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -404,6 +404,7 @@ ActiveRecord::Schema.define(version: 20140630213356) do
     t.integer  "shipping_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "tax_cloud_tic"
   end
 
   add_index "spree_products", ["available_on"], name: "index_spree_products_on_available_on", using: :btree
