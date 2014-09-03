940d5c6ea3b581bfd4828a75ba3196c2a26f55ed
Essam Joubori <essam.joubori@gmail.com>
2014-09-03 17:02:26 -0500

    Added S3 to spree

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,9 +6,19 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
+
+  config.use_s3 = true
+  config.s3_bucket = "sweetandlashfulcom"
+  config.s3_access_key = "AKIAJHAA4VXZHNH7QNIA"
+  config.s3_secret = "wPBzYvuBtyePT+6DIr8tqgW4LsxvxDi81vUFqWWY"
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
 end
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
+
 
 Spree.user_class = "Spree::LegacyUser"
